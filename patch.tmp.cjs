d45a9dc3a45f796b68de94a085c7d35070e6aec4
HernanNET <hernan.net@gmail.com>
2026-09-19 12:33:18 +0200

    security: remove temporary debug endpoints

--- patch.tmp.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('node:fs');
-const path = 'D:/DiskC/Projects/python/AuthCore/dist/server/wrangler.json';
-const config = JSON.parse(fs.readFileSync(path, 'utf8'));
-config.compatibility_flags = ['nodejs_compat'];
-config.kv_namespaces = [{ binding: 'SESSION', id: '8776b774744e4d1ba4f6ac8307a02872' }];
-fs.writeFileSync(path, JSON.stringify(config, null, 2) + '\n');
-console.log('patched: flags=' + JSON.stringify(config.compatibility_flags) + ' kv=' + config.kv_namespaces[0].id);
